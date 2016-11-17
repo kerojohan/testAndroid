d09d777f18cb1b21c272f057e7ddf02e364bfe5e
Master <master@iMac-de-LaSalleBCN-7.local>
2016-11-17 21:01:27 +0100

    Shopping List

--- app/src/main/java/com/salle/master/test/MainActivity.java
+++ app/src/main/java/com/salle/master/test/MainActivity.java
@@ -1,33 +1,40 @@
 package com.salle.master.test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button=(Button) findViewById(R.id.buttonSend);
-        /*button.setOnClickListener(new View.OnClickListener() {
+        ListView shopList=(ListView) findViewById(R.id.shopList);
+        ArrayList<String> shopElements = new ArrayList<>();
+        shopElements.add("First");
+        shopElements.add("Second");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,shopElements);
+        shopList.setAdapter(arrayAdapter);
+
+        Button addButton = (Button) findViewById(R.id.addbutton);
+        addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText nameEditText=(EditText)findViewById(R.id.editText4);
-                System.out.println(nameEditText.getText());
+                Intent intent =new Intent(getApplicationContext(),FormActivity.class);
+                startActivity(intent);
             }
-        });*/
-
-        button.setOnClickListener(this);
+        });
 
     }
-    @Override
-    public void onClick(View view) {
-        EditText nameEditText=(EditText)findViewById(R.id.editText4);
-        System.out.println(nameEditText.getText());
-    }
+
 }
