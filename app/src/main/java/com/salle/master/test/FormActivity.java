3560c68f17b3556412d3e4bba87adaffbd42ccdc
Joan Caparrós <joan.caparros@csuc.cat>
2016-11-18 14:24:03 +0100

    ShopList with add option

--- app/src/main/java/com/salle/master/test/FormActivity.java
+++ app/src/main/java/com/salle/master/test/FormActivity.java
@@ -1,18 +1,35 @@
 package com.salle.master.test;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class FormActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.form_main);
+
+        Button addbutton = (Button) findViewById(R.id.insertbutton);
+
+        addbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent resultIntent = new Intent();
+                EditText edittext = (EditText) findViewById(R.id.elementtoadd);
+                resultIntent.putExtra("newitem", edittext.getText().toString() );
+                setResult(Activity.RESULT_OK,resultIntent);
+                finish();
+            }
+        });
     }
 
 }
--- app/src/main/java/com/salle/master/test/MainActivity.java
+++ app/src/main/java/com/salle/master/test/MainActivity.java
@@ -1,5 +1,6 @@
 package com.salle.master.test;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,18 +13,20 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity{
+    private ArrayList<String> shopElements = new ArrayList<>();
+    private ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,shopElements);
 
         ListView shopList=(ListView) findViewById(R.id.shopList);
-        ArrayList<String> shopElements = new ArrayList<>();
         shopElements.add("First");
         shopElements.add("Second");
+        shopElements.add("Third");
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,shopElements);
         shopList.setAdapter(arrayAdapter);
 
         Button addButton = (Button) findViewById(R.id.addbutton);
@@ -31,10 +34,25 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onClick(View view) {
                 Intent intent =new Intent(getApplicationContext(),FormActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent,1);
             }
         });
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            // Make sure the request was successful
+        if (requestCode == 1) {
+            if(resultCode == Activity.RESULT_OK){
+                String itemtoadd=data.getStringExtra("newitem");
+                // The user picked a contact.
+                System.out.println(itemtoadd);
+                shopElements.add(itemtoadd);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            }
     }
 
 }
